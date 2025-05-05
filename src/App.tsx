771f5674a414a3357da87c50d62d673ffd35f02a
M1ndRush <114644188+M1ndRush@users.noreply.github.com>
2025-05-05 22:55:56 +0300

    Добавлен тест с датой для проверки деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div style={{ color: 'red', fontSize: 40 }}>Тестовый контент</div>;
+  return <div style={{ color: 'red', fontSize: 40 }}>
+    Тестовый контент - {new Date().toLocaleString()}
+  </div>;
 }
 
 export default App;
