1283efb52f0b390a372769abb0e1d2f87a9f04ef
M1ndRush <114644188+M1ndRush@users.noreply.github.com>
2025-05-05 23:18:22 +0300

    Добавлена иконка и убран dist из .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import SortingGame from './pages/SortingGame';
+import ColorGame from './pages/ColorGame';
+import MemoryGame from './pages/MemoryGame';
+import { ScoreProvider } from './contexts/ScoreContext';
 
 function App() {
-  return <div style={{ color: 'red', fontSize: 40 }}>
-    Тестовый контент - {new Date().toLocaleString()}
-    <br />
-    Проверка деплоя в gh-pages
-  </div>;
+  return (
+    <ScoreProvider>
+      <div className="min-h-screen bg-gradient-to-b from-sky-100 to-purple-100 flex flex-col">
+        <Header />
+        <main className="flex-1 container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sorting" element={<SortingGame />} />
+            <Route path="/color" element={<ColorGame />} />
+            <Route path="/memory" element={<MemoryGame />} />
+          </Routes>
+        </main>
+      </div>
+    </ScoreProvider>
+  );
 }
 
 export default App;
