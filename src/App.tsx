331ac0a8ea2df33a1118cdd44e1fabde40d8d0a1
M1ndRush <114644188+M1ndRush@users.noreply.github.com>
2025-05-05 22:28:06 +0300

    hotfix with test content

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import SortingGame from './pages/SortingGame';
-import ColorGame from './pages/ColorGame';
-import MemoryGame from './pages/MemoryGame';
-import { ScoreProvider } from './contexts/ScoreContext';
 
 function App() {
-  const location = useLocation();
-  const isHome = location.pathname === '/' || location.pathname === '/KidGamesKZ/' || location.pathname === '';
-  return (
-    <ScoreProvider>
-      <div className="min-h-screen bg-gradient-to-b from-sky-100 to-purple-100 flex flex-col">
-        <Header />
-        <main className="flex-1 container mx-auto px-4 py-8">
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="/sorting" element={<SortingGame />} />
-            <Route path="/color" element={<ColorGame />} />
-            <Route path="/memory" element={<MemoryGame />} />
-            <Route path="*" element={<HomePage />} />
-          </Routes>
-        </main>
-        {isHome && (
-          <footer className="bg-indigo-600 text-white text-center py-4">
-            <p>Балаларға арналған дамытушы ойындар &copy; 2025</p>
-          </footer>
-        )}
-      </div>
-    </ScoreProvider>
-  );
+  return <div style={{ color: 'red', fontSize: 40 }}>Тестовый контент</div>;
 }
 
 export default App;
