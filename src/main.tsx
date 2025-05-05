cf02468ce18e78717a4dd834b28119a051452404
M1ndRush <114644188+M1ndRush@users.noreply.github.com>
2025-05-05 22:41:18 +0300

    hotfix 6

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,7 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { LanguageProvider } from './contexts/LanguageContext';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <HashRouter>
-      <LanguageProvider>
-        <App />
-      </LanguageProvider>
-    </HashRouter>
-  </StrictMode>
+  <App />
 );
